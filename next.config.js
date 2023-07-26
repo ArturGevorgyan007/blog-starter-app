88d244ef923edaf3f8db0ba6f81054f661e9f9d3
Artur <arturgevorgyan87@yahoo.com>
2023-07-25 19:04:03 -0700

    Edited package.json

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
-const withMDX = require('@next/mdx')({
-    extension: /\.mdx?$/,
-  })
-  
-  module.exports = withMDX({
-    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-    reactStrictMode: true,
-    trailingSlash: true,
-    swcMinify: true,
+module.exports = {
     images: {
-      unoptimized: true
+        unoptimized: true
     }
-  })
+}
