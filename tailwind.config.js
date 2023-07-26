404dc0c293c024b6c78c4421ed591869523d0a3c
Artur <arturgevorgyan87@yahoo.com>
2023-07-25 18:12:29 -0700

    Edited package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./components/**/*.tsx', './pages/**/*.tsx'],
+  images: {
+    unoptimized: true
+  },
   theme: {
     extend: {
       colors: {
